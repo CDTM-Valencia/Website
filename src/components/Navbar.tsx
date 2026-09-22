5e863a4ad3d1f95cbefd03847bce9d9c6ace57d2
diego-manuel1 <dmfg10102@gmail.com>
2026-09-22 13:10:52 +0200

    Remove projects temporary from navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -98,7 +98,7 @@ const Navbar = ({ backgroundColor = 'bg-transparent' }: NavbarProps) => {
           { id: 'speakers', label: 'Speakers', url: '#speakers' },
           { id: 'prizes', label: 'Prizes', url: '#prizes' },
           { id: 'about-us', label: 'Organisers', url: '#about-us' },
-          { id: 'projects', label: 'Projects', url: '/projects/2025' },
+          //{ id: 'projects', label: 'Projects', url: '/projects/2025' },
           //{ id: 'projects', label: 'Projects', url: '/projects/2025' },
           {
             id: 'github',
@@ -165,12 +165,12 @@ const Navbar = ({ backgroundColor = 'bg-transparent' }: NavbarProps) => {
             Organisers
           </button>
 
-          <a
+          {/*<a
             href="/projects/2025"
             className="text-springText/80 hover:text-springBlue transition-colors flex items-center gap-2"
           >
             Projects
-          </a>
+          </a>*/}
           <a
             href="https://github.com/CDTM/cdtm-hacks"
             target="_blank"
